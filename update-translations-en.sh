39b30e283d104b60c9805739f4e74c72037bcb1d
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-08-15 00:00:00 +0000

    zzz

--- update-translations-en.sh
+++ update-translations-en.sh
@@ -6,3 +6,9 @@ set -Eeuxo pipefail
 pybabel extract --omit-header -F babel.cfg -o messages.pot .
 pybabel update -l en --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
 pybabel compile -l en -f -d allthethings/translations
+pybabel extract --omit-header -F babel.cfg -o messages.pot .
+pybabel update -l en --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
+pybabel compile -l en -f -d allthethings/translations
+pybabel extract --omit-header -F babel.cfg -o messages.pot .
+pybabel update -l en --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
+pybabel compile -l en -f -d allthethings/translations
